29e2b67fd186d328f7a12376a24b75afd20ab285
kelven14 <kelven.cleiton14@gmail.com>
2021-01-21 13:16:44 -0300

    Project created

---
+++ backend/src/main/java/com/linkedreams/connectDreams/ConnectDreamsApplication.java
@@ -0,0 +1,13 @@
+package com.linkedreams.connectDreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectDreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectDreamsApplication.class, args);
+	}
+
+}
