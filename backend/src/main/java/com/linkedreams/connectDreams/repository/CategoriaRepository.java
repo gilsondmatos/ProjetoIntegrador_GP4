6d4b3e12ba618a7c7b5814ec1068b02741848a4e
kiko <kikopesqueira96@gmail.com>
2021-01-29 15:19:13 -0300

    criação do crud da entidade categoria

---
+++ backend/src/main/java/com/linkedreams/connectDreams/repository/CategoriaRepository.java
@@ -0,0 +1,21 @@
+package com.linkedreams.connectDreams.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.linkedreams.connectDreams.model.Categoria;
+
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria ,Long> {
+	
+	public List<Categoria> findAllByNomeContainingIgnoreCase (String nome);
+	
+	//public List<Categoria> findAllByCausaContainingIgnoreCase (String causa);
+
+		
+	
+	
+}
