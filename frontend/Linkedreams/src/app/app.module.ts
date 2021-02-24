0226f2b6faac9cda8021cb0d8cd1f8de3af9159a
gilsondmatos <gilsondmatos@hotmail.com>
2021-02-24 10:25:06 -0300

    Implmentação inicial

--- frontend/Linkedreams/src/app/app.module.ts
+++ frontend/Linkedreams/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { OngComponent } from './ong/ong.component';
+import { ApoiadorComponent } from './apoiador/apoiador.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    OngComponent,
+    ApoiadorComponent
   ],
   imports: [
     BrowserModule,
