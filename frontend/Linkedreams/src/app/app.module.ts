725cc3ce27d0dadc7f4985ab74ef045ad8898e4b
erufluk <erufluk@gmail.com>
2021-02-24 19:05:41 -0300

    Implementação tela de pagamento

--- frontend/Linkedreams/src/app/app.module.ts
+++ frontend/Linkedreams/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { PagamentoComponent } from './pagamento/pagamento.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RodapeComponent
-  ],
+    RodapeComponent,
+    PagamentoComponent,
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule
