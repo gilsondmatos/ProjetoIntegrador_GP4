cd2433a1d66330fd098124099ef1183aacc73c83
kelven14 <kelven.cleiton14@gmail.com>
2021-02-24 18:40:39 -0300

    Correção de conflitos2

--- frontend/Linkedreams/src/app/app.module.ts
+++ frontend/Linkedreams/src/app/app.module.ts
@@ -5,14 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
-import { CadastroProdutoComponent } from './cadastro-produto/cadastro-produto.component';
+import { OngComponent } from './ong/ong.component';
+import { ApoiadorComponent } from './apoiador/apoiador.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     RodapeComponent,
-    CadastroProdutoComponent
+    OngComponent,
+    ApoiadorComponent
   ],
   imports: [
     BrowserModule,
