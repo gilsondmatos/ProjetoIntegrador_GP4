d4362a66b4066da70a8ebae1d32336a7e9566c9d
kelven14 <kelven.cleiton14@gmail.com>
2021-03-03 14:09:14 -0300

    ATUALIZAÇÂO DOS ARQUIVOS MODEL, CONTROLLER , REPOSITORY E SECURITY

--- backend/src/main/java/com/linkedreams/connectDreams/repository/ProdutoRepository.java
+++ backend/src/main/java/com/linkedreams/connectDreams/repository/ProdutoRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.linkedreams.connectDreams.model.Categoria;
 import com.linkedreams.connectDreams.model.Produto;
 
 
@@ -12,6 +13,8 @@ import com.linkedreams.connectDreams.model.Produto;
 public interface ProdutoRepository extends JpaRepository<Produto ,Long> {
 	
 	public List<Produto> findAllByNomeContainingIgnoreCase (String nome);
+	
+	public List<Produto> findAllByCausaContainingIgnoreCase (String causa);
 	
 }
 		
